24ea52007a65ad87ec614a3d2c654ce88dfa143b
nskowron <nel.skowronek@gmail.com>
2025-01-13 22:04:40 +0000

    fixed foreign key constraints

--- list5OLD/db.py
+++ list5OLD/db.py
@@ -1,10 +1,13 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
 # Initialize database
 # Set up the engine and session
 engine = create_engine('sqlite:///library.db', echo=True)
+with engine.connect() as connection:
+    connection.execute(text("PRAGMA foreign_keys = ON;"))
+
 Session = sessionmaker(bind=engine)
 
 # Create tables
